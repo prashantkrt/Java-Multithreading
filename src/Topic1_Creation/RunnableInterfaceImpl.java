814736b7a53bdaa4921df6db42b857fb514bd0a1
prashant <prashanttiwary10@gmail.com>
2024-12-02 23:22:31 +0530

    Creation of Thread

--- src/Topic1_Creation/RunnableInterfaceImpl.java
+++ src/Topic1_Creation/RunnableInterfaceImpl.java
@@ -11,9 +11,24 @@ public class RunnableInterfaceImpl implements Runnable {
         }
     }
 
+
     public static void main(String[] args) {
         RunnableInterfaceImpl runnableInterface = new RunnableInterfaceImpl();
         Thread thread = new Thread(runnableInterface);
         thread.start();
     }
 }
+/*
+* Life cycle of the Thread
+* New: The thread is created but hasn’t started yet. Like => Thread thread = new Thread();
+* Runnable : After calling start(), the thread enters the runnable state. The thread is eligible to run but may not execute immediately due to the thread scheduler. Like => thread.start();
+* Running : The thread is actively executing its run() method.
+* Blocked: The thread is waiting to acquire a lock or resource that another thread is holding. Example: When a thread tries to enter a synchronized block but the lock is held by another thread.
+* Waiting: The thread is waiting indefinitely until another thread signals it to continue using methods like Object.wait().
+*     synchronized (obj) {
+*        obj.wait(); // Thread waits indefinitely
+*      }
+* Timed_Wait: The thread is waiting for a specified period using methods like Thread.sleep() , Thread.join()
+* Terminated :The thread has finished executing its task and is no longer active. Example: When the run() method completes or an uncaught exception terminates the thread.
+*
+* */
--- src/Topic1_Creation/ThreadClassImpl.java
+++ src/Topic1_Creation/ThreadClassImpl.java
@@ -1,8 +1,10 @@
 package Topic1_Creation;
-// First way Extend the Thread class
+// First way Extend the Thread class,
 // we can directly call start method from The Thread class object
 public class ThreadClassImpl extends Thread{
 
+
+    //Run method has the code which will be working in executed in multiple threads
     @Override
     public void run(){
         for (int i = 0; i < 10; i++) {
@@ -15,3 +17,17 @@ public class ThreadClassImpl extends Thread{
         myThread.start();
     }
 }
+/*
+ * Life cycle of the Thread
+ * New: The thread is created but hasn’t started yet. Like => Thread thread = new Thread();
+ * Runnable : After calling start(), the thread enters the runnable state. The thread is eligible to run but may not execute immediately due to the thread scheduler. Like => thread.start();
+ * Running : The thread is actively executing its run() method.
+ * Blocked: The thread is waiting to acquire a lock or resource that another thread is holding. Example: When a thread tries to enter a synchronized block but the lock is held by another thread.
+ * Waiting: The thread is waiting indefinitely until another thread signals it to continue using methods like Object.wait().
+ *     synchronized (obj) {
+ *        obj.wait(); // Thread waits indefinitely
+ *      }
+ * Timed_Wait: The thread is waiting for a specified period using methods like Thread.sleep() , Thread.join()
+ * Terminated :The thread has finished executing its task and is no longer active. Example: When the run() method completes or an uncaught exception terminates the thread.
+ *
+ * */
