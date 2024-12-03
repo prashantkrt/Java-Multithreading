61a33a51194ebbb400c87257fc21696caba3dc3f
prashant <prashanttiwary10@gmail.com>
2024-12-03 23:24:33 +0530

    Synchronization

---
+++ src/Topic3_Synchronization/StaticSynchronizedExample.java
@@ -0,0 +1,54 @@
+package Topic3_Synchronization;
+
+class Counter2 {
+
+    private static int count = 0;
+
+    // Synchronized static method to ensure thread-safety at the class level
+    public synchronized static void increment() {
+        count++;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+}
+
+class MyThread2 extends Thread {
+
+    private Counter2 counter;
+
+    public MyThread2(Counter2 counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            Counter2.increment();
+        }
+    }
+}
+
+public class StaticSynchronizedExample {
+    public static void main(String[] args) {
+
+        Counter2 counter = new Counter2();
+        MyThread2 myThread1 = new MyThread2(counter);
+        myThread1.start();
+
+        Counter2 counter2 = new Counter2();
+        MyThread2 myThread2 = new MyThread2(counter2);
+        myThread2.start();
+
+        try {
+            myThread1.join();
+            myThread2.join();
+        } catch (InterruptedException ignored) {
+        }
+
+        System.out.println(Counter2.getCount());
+
+
+    }
+}
---
+++ src/Topic3_Synchronization/SynchronizedExample.java
@@ -0,0 +1,59 @@
+package Topic3_Synchronization;
+
+class Counter {
+
+    private int count;
+
+    public Counter() {
+        count = 0;
+    }
+
+    // Object level
+    // Only one thread can access the synchronized method on that particular object at a time.
+    // The synchronized block in the increment() method ensures that only one thread at a time can modify the count variable.
+    // Synchronized method to ensure thread-safety
+    // Only one thread at a time
+    // Other thread will be blocked or wait till the thread will complete the task
+    synchronized public void increment() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
+class MyThread extends Thread {
+
+    private Counter counter;
+
+    public MyThread(Counter counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            counter.increment();
+        }
+    }
+}
+
+public class SynchronizedExample {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        MyThread myThread1 = new MyThread(counter);
+        MyThread myThread2 = new MyThread(counter);
+        myThread1.start();
+        myThread2.start();
+
+        try {
+            myThread1.join();
+            myThread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(counter.getCount());
+    }
+}
