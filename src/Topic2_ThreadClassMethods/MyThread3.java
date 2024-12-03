f296557900fa161c3ec35f32474e0c10947a797b
prashant <prashanttiwary10@gmail.com>
2024-12-03 22:23:11 +0530

    Thread Methods

--- src/Topic2_ThreadClassMethods/MyThread3.java
+++ src/Topic2_ThreadClassMethods/MyThread3.java
@@ -19,6 +19,7 @@ public class MyThread3 extends Thread {
     public static void main(String[] args) {
         MyThread3 myThread3 = new MyThread3("MyThread3");
         myThread3.start();
+        // Interrupts a thread, signaling it to stop its execution or terminate early (if it is sleeping or waiting).
         // main method interrupt kar degas once the MyThread ask it to do so
         myThread3.interrupt(); // singles the main or current thread => jo bhi kaam kar rahe ho usko wahi rok do
     }
--- src/Topic2_ThreadClassMethods/MyThread4.java
+++ src/Topic2_ThreadClassMethods/MyThread4.java
@@ -3,6 +3,19 @@ package Topic2_ThreadClassMethods;
 public class MyThread4 implements Runnable {
     @Override
     public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i+" is running");
+            //Hints to the scheduler that the current thread is willing to pause and allow other threads of the same priority to execute.
+            //Hints the scheduler to give change to another thread as well!!!
+            Thread.yield();// durse thread ko chance de do
+        }
+    }
 
+    public static void main(String[] args) {
+        MyThread4 myThread4 = new MyThread4();
+        Thread t1 = new Thread(myThread4);
+        Thread t2 = new Thread(myThread4);
+        t1.start();
+        t2.start();
     }
 }
