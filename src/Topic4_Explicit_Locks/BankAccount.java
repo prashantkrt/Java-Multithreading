9137101fc044f99dec957c04cc9be74dc46d3cc6
prashant <prashanttiwary10@gmail.com>
2024-12-04 22:56:21 +0530

    Locks

--- src/Topic4_Explicit_Locks/BankAccount.java
+++ src/Topic4_Explicit_Locks/BankAccount.java
@@ -1,40 +1,42 @@
 package Topic4_Explicit_Locks;
 
+//Without lock
 public class BankAccount {
 
     private Integer balance;
 
-    public void withdraw(Integer amount) {
-        System.out.println(Thread.currentThread().getName() + ": withdrawing " + amount + " from bank");
+    //auto locking and will take over the entire method for each thread one by one
+    public synchronized void withdraw(Integer amount) {
+        System.out.println(Thread.currentThread().getName() + " : withdrawing " + amount + " from bank");
         if (amount <= balance) {
             balance = balance - amount;
             try {
-                System.out.println(Thread.currentThread().getName() + ": Processing the withdrawal request");
+                System.out.println(Thread.currentThread().getName() + " : Processing the withdrawal request");
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            System.out.println(Thread.currentThread().getName() + ": withdrawing " + amount + " from bank");
-            System.out.println(Thread.currentThread().getName() +": Current balance: " + balance);
-            System.out.println(Thread.currentThread().getName() +": Withdrawn amount: " + amount);
-            System.out.println(Thread.currentThread().getName() +": Successfully withdrawn " + amount + " from bank");
+            System.out.println(Thread.currentThread().getName() + " : withdrawing " + amount + " from bank");
+            System.out.println(Thread.currentThread().getName() +" : Current balance: " + balance);
+            System.out.println(Thread.currentThread().getName() +" : Withdrawn amount: " + amount);
+            System.out.println(Thread.currentThread().getName() +" : Successfully withdrawn " + amount + " from bank");
         } else {
-            System.out.println(Thread.currentThread().getName() +": Insufficient balance");
+            System.out.println(Thread.currentThread().getName() +" : Insufficient balance");
         }
     }
 
     public void deposit(Integer amount) {
-        System.out.println(Thread.currentThread().getName() +": deposit " + amount + " from bank");
+        System.out.println(Thread.currentThread().getName() +" : deposit " + amount + " from bank");
         if (amount > 0) {
             balance = balance + amount;
             try {
-                System.out.println(Thread.currentThread().getName() +": Processing the deposit request");
+                System.out.println(Thread.currentThread().getName() +" : Processing the deposit request");
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         } else {
-            System.out.println(Thread.currentThread().getName() +": Insufficient amount to deposit");
+            System.out.println(Thread.currentThread().getName() +" : Insufficient amount to deposit");
         }
     }
 
@@ -50,14 +52,14 @@ public class BankAccount {
 class Main {
     public static void main(String[] args) {
         BankAccount bankAccount = new BankAccount();
-        bankAccount.setBalance(100);
+        bankAccount.setBalance(200);
 
         Thread thread1 = new Thread(() -> {
-            bankAccount.withdraw(10);
+            bankAccount.withdraw(20);
         }, "Thread1");
 
         Thread thread2 = new Thread(() -> {
-            bankAccount.withdraw(10);
+            bankAccount.withdraw(20);
         }, "Thread2");
 
         thread1.start();
---
+++ src/Topic4_Explicit_Locks/BankAccountLock.java
@@ -0,0 +1,97 @@
+package Topic4_Explicit_Locks;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+// Synchronized gives auto locking
+// Lock is an Interface
+// we will be using the lock for manual lock
+public class BankAccountLock {
+
+    private Integer balance;
+
+    private final Lock lock = new ReentrantLock();
+
+    public void withdraw(Integer amount) {
+        System.out.println(Thread.currentThread().getName() + ": withdrawing " + amount + " from bank");
+        if (lock.tryLock()) {
+            if (balance >= amount) {
+                balance = balance - amount;
+                try {
+                    System.out.println(Thread.currentThread().getName() + ": Processing the withdrawal request");
+                    Thread.sleep(2000);
+
+                    System.out.println(Thread.currentThread().getName() + ": withdrawing " + amount + " from bank");
+                    System.out.println(Thread.currentThread().getName() + ": Current balance: " + balance);
+                    System.out.println(Thread.currentThread().getName() + ": Withdrawn amount: " + amount);
+                    System.out.println(Thread.currentThread().getName() + ": Successfully withdrawn " + amount + " from bank");
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println(Thread.currentThread().getName() + ": Insufficient balance");
+            }
+        } else {
+            System.out.println(Thread.currentThread().getName() + ": Waiting in the queue for resource to be uncloaked acquired by another thread :)");
+        }
+    }
+
+    public void deposit(Integer amount) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + ": deposit " + amount + " from bank");
+        if (lock.tryLock(1000, TimeUnit.NANOSECONDS)) {
+            if (amount > 0) {
+                balance = balance + amount;
+                try {
+                    System.out.println(Thread.currentThread().getName() + " : Processing the deposit request");
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println(Thread.currentThread().getName() + " : Insufficient amount to deposit");
+            }
+        } else {
+            System.out.println(Thread.currentThread().getName() + ": Waiting in the queue for resource to be uncloaked acquired by another thread :)");
+        }
+    }
+
+    public void setBalance(Integer balance) {
+        this.balance = balance;
+    }
+
+    public Integer getBalance() {
+        return balance;
+    }
+}
+
+class MainLock {
+    public static void main(String[] args) {
+        BankAccountLock bankAccount = new BankAccountLock();
+        bankAccount.setBalance(100);
+
+        Thread thread1 = new Thread(() -> {
+            bankAccount.withdraw(10);
+        }, "Thread1");
+
+        Thread thread2 = new Thread(() -> {
+            bankAccount.withdraw(10);
+        }, "Thread2");
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(bankAccount.getBalance());
+    }
+}
