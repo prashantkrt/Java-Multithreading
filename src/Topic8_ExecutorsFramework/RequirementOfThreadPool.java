7aaa1af695c5cca7fccb894458d674a7259e01e1
prashant <prashanttiwary10@gmail.com>
2024-12-06 20:38:59 +0530

    ThreadPool Requirement

---
+++ src/Topic8_ExecutorsFramework/RequirementOfThreadPool.java
@@ -0,0 +1,62 @@
+package Topic8_ExecutorsFramework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RequirementOfThreadPool{
+
+    private static long calculateFactorial(int n) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        long factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+    public static void main(String[] args) {
+        //WithOut Threading
+        long start = System.currentTimeMillis();
+
+        for (int i = 0; i < 10; i++) {
+            long factorial = calculateFactorial(i);
+            System.out.println(factorial);
+        }
+
+        long end = System.currentTimeMillis();
+        // around 10-15 secs
+        System.out.println("Total Time of Execution without threading " + (end - start) / 1000 + " seconds");
+
+
+        // With Threading
+        long startTime = System.currentTimeMillis();
+
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            int finalI = i;
+            Thread thread = new Thread(() -> {
+                long l = calculateFactorial(finalI);
+                System.out.println(l);
+            });
+            thread.start();
+            threads.add(thread); // Keep track of the thread
+        }
+
+        // Join all threads
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        long endTime = System.currentTimeMillis();
+        //around 1 sec to complete
+        System.out.println("Total Time of Execution with threading: " + (endTime - startTime) / 1000 + " seconds");
+    }
+}
